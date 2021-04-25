a2f0fafa86bbe276a85d43226d32820432bb4f0b
cassluvs2code <2csporvida@gmail.com>
2021-04-25 16:24:48 -0700

    finished building my weather app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from  "react";
 import Weather from './components/weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
 export default function App() {
 
 	const [lat, setLat] = useState([]);
@@ -29,7 +30,11 @@ export default function App() {
 			{(typeof data.main != 'undefined') ? (
 				<Weather weatherData={data}/>
 			) : (
-				<div></div>
+				<div>
+					<Dimmer active>
+						<Loader>Loading..</Loader>
+					</Dimmer>
+				</div>
 			)}
 
 		</div>
